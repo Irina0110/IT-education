afa3ec2797e7bd8bdd36c79d580e1bec130bd5bb
Irina0110 <irinasafonova2001@mail.ru>
2023-04-15 22:12:08 +0300

    feat: add function to show-more button

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './style.scss';
 import Recommendation from "./components/Recommendation";
 import IconDown from '../../static/icons/IconDown.svg';
+import IconUp from '../../static/icons/IconUp.svg';
 import FrontendWay from "./components/FrontendWay";
 import CatalogThemes from "./components/CatalogThemes";
 
 const Education = () => {
+    const [showButton, setStateButton] = useState(false);
+
+    const handleSetState = () => {
+        const hiddenContent = document.getElementById('hiddenContent');
+        if (hiddenContent) {
+            if(showButton){
+                hiddenContent.style.display = 'none';
+                setStateButton(false)
+            } else {
+                hiddenContent.style.display = 'block';
+                setStateButton(true)
+            }
+        }
+    }
 
     return (
         <React.Fragment>
@@ -14,13 +29,26 @@ const Education = () => {
                     <Recommendation/>
                     <div className='education__show-more'>
                         <h4>
-                            <button className='show-more'>Показать еще
-                                <img src={IconDown} alt='IconDown'/>
+                            <button className='show-more' onClick={handleSetState}>
+                                {!showButton && (
+                                    <div>
+                                        <span>Показать еще</span>
+                                        <img src={IconDown} alt='IconDown'/>
+                                    </div>
+                                )}
+                                {showButton && (
+                                    <div>
+                                        Скрыть
+                                        <img src={IconUp} alt='IconUp'/>
+                                    </div>
+                                )}
                             </button>
                         </h4>
                     </div>
-                    <FrontendWay/>
-                    <CatalogThemes/>
+                    <div id='hiddenContent'>
+                        <FrontendWay/>
+                        <CatalogThemes/>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
